c0197a29158fa93222b81b9b5c922109b0426bdf
Emily G <aspentree79@gmail.com>
2019-08-06 17:17:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,3 +7,15 @@ def square_array(array)
   end
   squared
 end
+
+
+
+def square_array(array)
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
+end
